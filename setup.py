7a90ff17a22a1dd3b27007e897c028a9cd75b991
ijknabla <ijknabla@gmail.com>
2023-07-20 23:52:11 +0900

    Bump supported arpeggio version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name="types-arpeggio",
-    version="0.0.0",
+    version="0.0.1.3-dev",
     packages=["arpeggio-stubs"],
     package_data={
         "arpeggio-stubs": ["*.pyi"],
     },
     install_requires=[
-        "arpeggio>=0.8",
+        "arpeggio>=1.3",
     ],
 )
