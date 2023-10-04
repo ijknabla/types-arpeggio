7b724ed475fc7197ea6b0f23f256001099bb7212
ijknabla <ijknabla@gmail.com>
2023-10-04 17:26:07 +0900

    Implement pos_agument check

--- tests/test_parser_python.py
+++ tests/test_parser_python.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from typing import TYPE_CHECKING, Any
 
 import pytest
-from arpeggio import EOF, ParserPython, Sequence
+from arpeggio import DEFAULT_WS, EOF, ParserPython, Sequence
 
 if TYPE_CHECKING:
     from arpeggio import _ParsingExpressionLike, _SyntaxClasses
@@ -39,19 +39,43 @@ def pos_arguments_generator(
     args,
     comment_def,
     syntax_classes,
+    skipws,
+    ws,
+    reduce_tree,
+    autokwd,
+    ignore_case,
+    memoization,
     """,
     pos_arguments_generator(
         (None, empty),
         ({}, {"Sequence": Sequence}),
+        (True, False),
+        (DEFAULT_WS, ""),
+        (False, True),
+        (False, True),
+        (False, True),
+        (False, True),
     ),
 )
 def test_parser_python_init_args(
     args: tuple[Any, ...],
-    comment_def: _ParsingExpressionLike,
-    syntax_classes: _SyntaxClasses,
+    comment_def: _ParsingExpressionLike | None,
+    syntax_classes: _SyntaxClasses | None,
+    skipws: bool,
+    ws: str | None,
+    reduce_tree: bool,
+    autokwd: bool,
+    ignore_case: bool,
+    memoization: bool,
 ) -> None:
     parser = ParserPython(empty, *args)
     assert parser.syntax_classes == syntax_classes
+    assert parser.skipws == skipws
+    assert parser.ws == ws
+    assert parser.reduce_tree == reduce_tree
+    assert parser.autokwd == autokwd
+    assert parser.ignore_case == ignore_case
+    assert parser.memoization == memoization
 
 
 @pytest.mark.parametrize(
