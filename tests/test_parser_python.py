adea89a62d923864c7bff5a31f8b9ff7a45071f9
ijknabla <ijknabla@gmail.com>
2023-10-04 18:28:05 +0900

    Complete ParserPython argument check

--- tests/test_parser_python.py
+++ tests/test_parser_python.py
@@ -38,49 +38,6 @@ file_values = DefaultAndValues[IO[str]](sys.stdout, [sys.stderr])
 
 
 @pytest.mark.parametrize(
-    "args,"
-    "comment_def,"
-    "syntax_classes,"
-    "skipws,"
-    "ws,"
-    "reduce_tree,"
-    "autokwd,"
-    "ignore_case,"
-    "memoization,",
-    iter_pos_args(
-        comment_def_values,
-        syntax_classes_values,
-        skipws_values,
-        ws_values,
-        reduce_tree_values,
-        autokwd_values,
-        ignore_case_values,
-        memoization_values,
-    ),
-)
-def test_parser_python_positional_arguments(
-    args: tuple[Any, ...],
-    comment_def: _ParsingExpressionLike | None,
-    syntax_classes: _SyntaxClasses,
-    skipws: bool,
-    ws: str,
-    reduce_tree: bool,
-    autokwd: bool,
-    ignore_case: bool,
-    memoization: bool,
-) -> None:
-    parser = ParserPython(empty, *args)
-    assert (parser.comments_model is None) == (comment_def is None)
-    assert parser.syntax_classes == syntax_classes
-    assert parser.skipws == skipws
-    assert parser.ws == ws
-    assert parser.reduce_tree == reduce_tree
-    assert parser.autokwd == autokwd
-    assert parser.ignore_case == ignore_case
-    assert parser.memoization == memoization
-
-
-@pytest.mark.parametrize(
     "kwargs,"
     "comment_def,"
     "syntax_classes,"
@@ -120,6 +77,62 @@ def test_parser_python_keyword_arguments(
     file: IO[str],
 ) -> None:
     parser = ParserPython(empty, **kwargs)
+    assert (parser.comments_model is None) == (comment_def is None)
+    assert parser.syntax_classes == syntax_classes
+    assert parser.skipws == skipws
+    assert parser.ws == ws
+    assert parser.reduce_tree == reduce_tree
+    assert parser.autokwd == autokwd
+    assert parser.ignore_case == ignore_case
+    assert parser.memoization == memoization
+    assert parser.debug == debug
+    assert parser.file == file
+
+
+@pytest.mark.parametrize(
+    "args,"
+    "comment_def,"
+    "syntax_classes,"
+    "skipws,"
+    "ws,"
+    "reduce_tree,"
+    "autokwd,"
+    "ignore_case,"
+    "memoization,",
+    iter_pos_args(
+        comment_def_values,
+        syntax_classes_values,
+        skipws_values,
+        ws_values,
+        reduce_tree_values,
+        autokwd_values,
+        ignore_case_values,
+        memoization_values,
+    ),
+)
+@pytest.mark.parametrize(
+    "kwargs," "debug," "file,",
+    iter_kw_args(
+        debug=debug_values,
+        file=file_values,
+    ),
+)
+def test_parser_python_positional_arguments(
+    tempdir: Path,
+    args: tuple[Any, ...],
+    kwargs: dict[str, Any],
+    comment_def: _ParsingExpressionLike | None,
+    syntax_classes: _SyntaxClasses,
+    skipws: bool,
+    ws: str,
+    reduce_tree: bool,
+    autokwd: bool,
+    ignore_case: bool,
+    memoization: bool,
+    debug: bool,
+    file: IO[str],
+) -> None:
+    parser = ParserPython(empty, *args, **kwargs)
     assert (parser.comments_model is None) == (comment_def is None)
     assert parser.syntax_classes == syntax_classes
     assert parser.skipws == skipws
